cae6bbd93692a5089e0d891f2c6fae0de7a1f2e8
zhangxin <915466@gmail.com>
2021-08-11 19:36:47 +0800

    update orm

--- buildSrc/src/main/kotlin/okt-web-orm-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/okt-web-orm-conventions.gradle.kts
@@ -5,5 +5,21 @@
 plugins {
     // Apply the common convention plugin for shared build configuration between library and application projects.
     id("okt-web-common-conventions")
+}
+dependencies {
+    // mysql
+    implementation("mysql:mysql-connector-java:$mysqlVersion")
 
+    // hikari
+    implementation("com.zaxxer:HikariCP:$hikariVersion")
+
+    // exposed
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+
+    //reflections
+    implementation("org.reflections:reflections:$reflectionsVersion")
+    //JColor
+    implementation("com.diogonunes:JColor:$jColorVersion")
 }

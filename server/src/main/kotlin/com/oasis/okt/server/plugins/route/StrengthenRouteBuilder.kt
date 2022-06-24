79e6d07b26e443c56578ee843efd553e219d6a96
zxc <44724385@qq.com>
2022-06-24 17:57:02 +0800

    增加路由消耗时间日志

--- server/src/main/kotlin/com/oasis/okt/server/plugins/route/StrengthenRouteBuilder.kt
+++ server/src/main/kotlin/com/oasis/okt/server/plugins/route/StrengthenRouteBuilder.kt
@@ -2,6 +2,7 @@
 
 package com.oasis.okt.server.plugins.route
 
+import com.oasis.okt.server.kernel.minfo
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.routing.*
@@ -12,7 +13,21 @@ fun Route.childRoute(
         method: HttpMethod,
         requiredRoles: List<String> = listOf("") ,
         body: PipelineInterceptor<Unit, ApplicationCall>) :Route {
-    val sub = route(path,method) { handle(body)}
+    val sub = route(path,method) {
+        handle {
+            System.currentTimeMillis().let { from ->
+                body.invoke(this,this.subject)
+                (System.currentTimeMillis() - from).let { cost ->
+                    if (cost > 100) {
+                        minfo("(slow-cost-time) path:$path,cost=${cost}ms")
+                    }
+                    else {
+                        minfo("(cost-time) path:$path,cost=${cost}ms")
+                    }
+                }
+            }
+        }
+    }
     application.routeDefinitions.setRequiredRolesRecursively(sub, requiredRoles)
     return sub
 }
